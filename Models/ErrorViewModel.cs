765a1e06c1488883222cb4fcd15f10c5f39c63fb
AbdullahNoman4146 <abdullaalnoman270@gmail.com>
2026-08-20 22:26:52 +0600

    Initial ASP.NET Core project setup

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ADHUNIK_BARI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
