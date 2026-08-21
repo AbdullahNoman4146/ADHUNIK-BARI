e26ad29a58b2bcd01bbb424de3a6ac8a8cd681f1
AbdullahNoman4146 <abdullaalnoman270@gmail.com>
2026-08-21 11:07:44 +0600

    Implemented authentication, roles and manager dashboard

---
+++ Controllers/ManagerController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace ADHUNIK_BARI.Controllers
+{
+
+    [Authorize(Roles = "Manager")]
+    public class ManagerController : Controller
+    {
+
+
+        public IActionResult Dashboard()
+        {
+
+            return View();
+
+        }
+
+
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,29 +1,89 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using ADHUNIK_BARI.Data;
+using ADHUNIK_BARI.Models;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+options.UseSqlServer(
+builder.Configuration.GetConnectionString("DefaultConnection")
+));
+
+
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+
+.AddEntityFrameworkStores<ApplicationDbContext>()
+
+.AddDefaultTokenProviders();
+
+
+
 builder.Services.AddControllersWithViews();
+
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
+
 if (!app.Environment.IsDevelopment())
 {
+
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+
 }
 
-app.UseHttpsRedirection();
+
+
+app.UseStaticFiles();
+
+
+
 app.UseRouting();
+
+
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
-app.MapStaticAssets();
+using (var scope = app.Services.CreateScope())
+{
+
+    var services = scope.ServiceProvider;
+
+
+    var userManager =
+    services.GetRequiredService<UserManager<ApplicationUser>>();
+
+
+    var roleManager =
+    services.GetRequiredService<RoleManager<IdentityRole>>();
+
+
+
+    await DbInitializer.SeedRolesAndAdmin(
+    userManager,
+    roleManager,
+    builder.Configuration
+);
+
+
+}
 
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
+
+name: "default",
+
+pattern: "{controller=Home}/{action=Index}/{id?}"
+
+);
+
 
 
 app.Run();
